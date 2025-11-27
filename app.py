998901ca0214c42b89727a2a3d0a48c367f0f979
Hutch2064 <achutchison02@gmail.com>
2025-11-27 14:10:49 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import streamlit as st
 # CONFIG
 # ============================================
 
-DEFAULT_START_DATE = "2011-11-24"
+DEFAULT_START_DATE = "2000-01-01"
 RISK_FREE_RATE = 0.0
 
 RISK_ON_WEIGHTS = {
