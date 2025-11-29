7c42a4b98e00ad392da111d787efa7f5761a75f4
Hutch2064 <achutchison02@gmail.com>
2025-11-28 18:21:43 -0600

    Update app.py

--- app.py
+++ app.py
@@ -188,7 +188,7 @@ def run_grid_search(prices, risk_on_weights, risk_off_weights):
 
     lengths = list(range(21, 253))
     types = ["sma", "ema"]
-    tolerances = np.arange(0.0, 0.1001, 0.002)
+    tolerances = np.arange(0.0, .2001, 0.0025)
 
     progress = st.progress(0.0)
     total = len(lengths) * len(types) * len(tolerances)
